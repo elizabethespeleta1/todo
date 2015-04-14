3597774cd7b802e8361dc80decc47cd2328e089b
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2015-04-14 11:06:20 -0700

    finished video 4

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+	//why is this important? Why does localhost have to be first?
+	$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+
+	//if this happens $mysqli->connect_error then we want it to die and have this message
+	if($mysqli->connect_error){
+		die('Connect Error (' . $mysqli->connect_errno . ')' 
+		. $mysqli->connect_error);
+	}
+	else{
+		echo "Connection made";
+	}
+	$mysqli->close();
+
+?>
